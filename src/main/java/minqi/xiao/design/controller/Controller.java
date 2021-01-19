bb1c97fb5591a0880b4a6e31324aa95ea535e1b5
chicken-noodle <1091203156@qq.com>
2021-01-19 12:44:26 +0800

    hello world

--- src/main/java/minqi/xiao/design/controller/Controller.java
+++ src/main/java/minqi/xiao/design/controller/Controller.java
@@ -1,6 +1,13 @@
 package minqi.xiao.design.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @org.springframework.stereotype.Controller
 public class Controller {
-    
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world!";
+    }
 }
